2c03211f23ed08005b15b85aedf95d4c69c30658
Dhrimanti <dhritimancg@gmail.com>
2025-11-21 11:25:09 +0530

    pls

--- socket.c
+++ socket.c
@@ -11,20 +11,27 @@ int main(){
     int sockfd=socket(AF_INET,SOCK_STREAM,0);
     struct sockaddr_in server_addr={
         .sin_family=AF_INET,
-        .sin_port=htons(9999),
-        .sin_addr.s_addr=inet_addr("127.0.0.1")
+        .sin_port=htons(80),
+        .sin_addr.s_addr=inet_addr("74.125.24.100")
     };
 
-    char* ip="127.0.0.1";
+    char* ip="74.125.24.100";
 
 
 
-    inet_pton(AF_INET,ip,&server_addr.sin_addr);
-    int res=connect(sockfd,(struct sockaddr*)&server_addr,sizeof(server_addr));
-    if(res==1){
-        printf("There is an error");
-    }else{
-        printf("There is no error and connected");
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+        perror("inet_pton failed");
+        close(sockfd);
+        return -1;
+    }
+
+    int res = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (res == -1) {  
+        perror("connect failed");
+        close(sockfd);
+        return -1;
+    } else {
+        printf("Connecting to server");
     }
 
 
