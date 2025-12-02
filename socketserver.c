dd38eb10761281c54ee3ae5ea6d4019e0195b9d9
Dhrimanti <dhritimancg@gmail.com>
2025-12-02 23:23:58 +0530

    I hope that i can die in a few days

---
+++ socketserver.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+int main(){
+    printf("Socket server code placeholder\n");
+    return 0;
+}
